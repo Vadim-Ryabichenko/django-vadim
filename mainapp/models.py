46809a66bd79453084fd41295eabeab3533a2685
Vadim <vadimrembyboy@gmail.com>
2023-11-22 15:31:01 +0200

    Third homework

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from topicsapp.models import Topic
 
-# Create your models here.
+
+class Article(models.Model):
+    name = models.CharField(max_length=255)
+    text = models.TextField(null=True, blank=True)
+    create_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+    topics = models.ManyToManyField(Topic)
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE, null = True, blank = True)
+
+    def __str__(self):
+        return f"Article_name is ~{self.name}~"
+
+
+class Comment(models.Model):
+    create_at = models.DateField(auto_now_add=True)
+    text = models.TextField(null=True, blank=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments", null = True, blank = True)
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE, null = True, blank = True)
+
+    def __str__(self):
+        return f"Comment created {self.create_at}, {self.text[:15]}"
+    
--- topicsapp/models.py
+++ topicsapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=64)
+    title = models.CharField(max_length=255, null=True, blank=True)
+    user = models.ManyToManyField("auth.User")
+
+    def __str__(self):
+        return f"Topic_name is ~{self.name}~"
