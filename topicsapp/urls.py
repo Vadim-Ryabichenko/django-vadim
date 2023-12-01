da4f58e0ea7157b3f9aecb2fee0676ea010bef2c
Vadim <vadimrembyboy@gmail.com>
2023-12-01 18:03:02 +0200

    fifth homework

--- topicsapp/urls.py
+++ topicsapp/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', topics_page),
-    path('<int:topic_id>/subscribe/', topic_subscribe),
+    path('<int:pk>/subscribe/', topic_subscribe, name = 'art_list'),
     path('<int:topic_id>/unsubscribe/', topic_unsubscribe),
 ]
--- topicsapp/views.py
+++ topicsapp/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Topic
+from mainapp.models import Article
 
 
 def topics_page(request):
     return HttpResponse("Hello, it`s page for topics")
 
-def topic_subscribe(request, topic_id):
-    return HttpResponse(f"Hello, it`s page for subscribe {topic_id} topic ")
+def topic_subscribe(request, pk):
+    topics = Topic.objects.filter(pk=pk)
+    articles = Article.objects.filter(topics__id = pk)
+    return render(request, 'art_list.html', {'topics' : topics, 'articles' : articles})
 
 def topic_unsubscribe(request, topic_id):
     return HttpResponse(f"Hello, it`s page for unsubscribe {topic_id} topics")
